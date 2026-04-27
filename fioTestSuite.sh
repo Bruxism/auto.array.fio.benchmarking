c4e67b8fe5e51acfef4b1878181ce8e70d3d421c
Bruxism <Bruxiandee@gmail.com>
2026-04-27 14:18:49 -0500

    Add curly braces to variables and move echos into main

--- fioTestSuite.sh
+++ fioTestSuite.sh
@@ -5,29 +5,30 @@
 set -euo pipefail
 shopt -s globstar nullglob
 
+
+
+# Drives to be tested
+DRIVES=(/dev/sd[a-m])
+
 # Check script location 
 ## This directory will be used in reference to the fio test files
 SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P)"
-echo "Location of script: $SCRIPT_DIR"
-
-# Array variable 'DRIVES'
-DRIVES=(/dev/sd[a-m])
-echo "Drive array initialized: ${DRIVES[@]}"
 
 # Initialize block device test file locations
-BLOCK_DEVICE_TESTS=($SCRIPT_DIR/BlockTests/**/*.fio)
-echo "Tests initialized: ${BLOCK_DEVICE_TESTS[@]}"
+BLOCK_DEVICE_TESTS=(${SCRIPT_DIR}/BlockTests/**/*.fio)
 
 # Set results directory by time
 RESULTS_DIR="fio.results-$(date +%Y.%m.%d-%H.%M-UTC)"
-echo "Results Directory: $RESULTS_DIR"
 
 # Function for listing drive information and saving it
 list_drives () {
-local filename="$RESULTS_DIR/drive_info.txt"
-echo "lsblk -o PATH,SIZE,MODEL,SERIAL,WWN" | tee $filename
-echo "" | tee -a $filename
-lsblk -o PATH,SIZE,MODEL,SERIAL,WWN ${DRIVES[@]} | tee -a $filename
+local filename="${RESULTS_DIR}/drive_info.txt"
+
+	{
+	echo "lsblk -o PATH,SIZE,MODEL,SERIAL,WWN"
+	echo ""
+	lsblk -o PATH,SIZE,MODEL,SERIAL,WWN ${DRIVES[@]}
+	} | tee -a "${filename}"
 }
 
 # Function for running fio tests
@@ -38,21 +39,24 @@ lsblk -o PATH,SIZE,MODEL,SERIAL,WWN ${DRIVES[@]} | tee -a $filename
 fio_tests () {
 for drive in "${DRIVES[@]}"; do
 	local serial="$(lsblk -no SERIAL $drive)"
-	local drive_results_dir="$RESULTS_DIR/$serial"
-	mkdir -p "$drive_results_dir"
+	local drive_results_dir="${RESULTS_DIR}/${serial}"
+	mkdir -p "${drive_results_dir}"
 	for test in "${BLOCK_DEVICE_TESTS[@]}"; do
-		local test_name="$(basename -s .fio $test)"
-		testlocation=$drive \
+		local test_name="$(basename -s .fio ${test})"
+		testlocation=${drive} \
 		fio \
 		--ramp_time=30s \
-		--output="$drive_results_dir/$test_name.txt" --output-format=normal,json \
-		$test
+		--output="${drive_results_dir}/${test_name}.txt" --output-format=normal,json \
+		${test}
 	done
 done
 }
 
 main () {
-mkdir $RESULTS_DIR
+echo "Location of script: ${SCRIPT_DIR}"
+echo "Tests initialized: ${BLOCK_DEVICE_TESTS[@]}"
+mkdir ${RESULTS_DIR}
+echo "Results Directory: ${RESULTS_DIR}"
 list_drives
 fio_tests
 }
