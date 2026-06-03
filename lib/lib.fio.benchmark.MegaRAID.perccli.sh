df32fa818dadd6e40997dc1381215b4b8bf1807a
Bruxism <Bruxiandee@gmail.com>
2026-06-02 19:19:24 -0500

    wip: functions for preparing hwraid + ext4 and iterating tests over it

--- lib/lib.fio.benchmark.MegaRAID.perccli.sh
+++ lib/lib.fio.benchmark.MegaRAID.perccli.sh
@@ -3,46 +3,124 @@
 #set location of MegaRAID perccli
 alias p='/opt/MegaRAID/perccli/perccli64'
 
-#DISK_HDD_SLOTS found in disk_list.sh
-##It's my personal list of drives by RAID slot number
-p /c0/e32/s"${DISK_HDD_SLOTS}" set good force
+# First section is RAID number, and the following list is the slots used
+declare -A HWRAID_LAYOUTS=(
+	[HDD8diskRAID0]="0 9,11,13,15,17,19,21,23"
+	[SSD4diskRAID0]="0 0,2,4,7"
+)
 
-#p /c0 add vd r0 name=8diskRAID0 drives=32:"${DISK_HDD_SLOTS}" pdcache=on wt nora direct
+hwraid_activate_disks() {
+p /c0/e32/s"${hwraid_all_slots_used}" set good force
+}
 
-p /c0 add vd r0 name=8diskRAID0 drives=32:"${DISK_HDD_SLOTS}" pdcache=off wb ra cached
+hwraid_collect_all_used_slots() {
+declare -g hwraid_all_slots_used
+declare -A hwraid_slots_used_array
+local some_slots_array some_slots some_slot
+
+for hwraid_array in "${HWRAID_LAYOUTS[@]}"; do
+	some_slots_array=()
+	some_slots="$(echo ${hwraid_array} | cut -d' ' -f2)"
+	IFS=, read -r -a some_slots_array <<< "${some_slots}"
+	for some_slot in "${some_slots_array[@]}"; do
+		declare -Ag hwraid_slots_used_array["${some_slot}"]=1
+	done
+done
+declare -g hwraid_all_slots_used=\
+"$(printf '%s\n' "${!hwraid_slots_used_array[@]}" | sort -n | paste -sd,)"
+}
+
+hwraid_resolve_direct() {
+case "${direct}" in
+	1)
+		pdcache=on
+		writeback=wt
+		readahead=nora
+		cachedirect=direct
+	;;
+	0)
+		pdcache=off
+		writeback=wb
+		readahead=ra
+		cachedirect=cached
+	;;
+esac
+}
+
+hwraid_disk_matrix() {
+hwraid_collect_all_used_slots
+hwraid_activate_disks
+hwraid_clear_virtual_disks
+for hwraid_array in "${!HWRAID_LAYOUTS[@]}"; do
+	disk_config="${hwraid_array}"
+	read -r raid_number hwraid_disk_slots <<<\
+		"${HWRAID_LAYOUTS[${hwraid_array}]}"
+	hwraid_test_matrix
+done
+}
+
+hwraid_add_virtual_disk() {
+	p /c0 add vd \
+	r"${raid_number}" \
+	name="${disk_config}" \
+	drives=32:"${hwraid_disk_slots}" \
+	pdcache="${pdcache}" \
+	"${writeback}"	\
+	"${readahead}" \
+	"${cachedirect}"
+}
+
+hwraid_test_matrix() {
+for direct in 1 0; do
+	[[ -n "${disk}" ]] && delete_ext4
+	hwraid_resolve_direct
+	hwraid_add_virtual_disk
+	disk=wwn-0x"$(hwraid_get_wwn)"
+	make_ext4
+	for profile in "${fio_profiles[@]}"; do
+		"${profile}"
+		# Skip psync when direct=0 since psync doesn't have direct=0
+		[[ " ${directs[@]} " =~ " ${direct} " ]] || continue
+		for blocksize in "${blocksizes[@]}"; do
+			for ((iodepth_i=0; iodepth_i<"${#iodepths[@]}"; iodepth_i++)); do
+				iodepth="${iodepths[iodepth_i]}"
+				numjobs="${numjobss[-1-iodepth_i]}"
+				for ioengine in "${ioengines[@]}"; do
+					if [[ "${ioengine}" == psync ]]; then
+						gtod_reduce=0
+					else
+						gtod_reduce=1
+					fi
+					for test_type in "${test_types[@]}"; do
+						for size in "${SIZES[@]}"; do
+						for runtime in "${RUNTIMES[@]}"; do
+						for use_pareto in "${use_paretos[@]}"; do
+							fio_function
+						done
+						done
+						done
+					done
+				done
+			done
+		done
+	done
+done
+}
 
 umount /dev/"${drive}"
 p /c0/v0 delete 
 
 p /c0/e32/s"${DISK_HDD_SLOTS}" set jbod
 
-#WWN_HDD_8DISK found in disk_list.sh
-##It's my personal list of drives by WWN id
-unset wwn8disk
-declare -a wwn8disk
-for wwn in "${WWN_HDD_8DISK[@]}"; do
-	wwn8disk+=("/dev/disk/by-id/wwn-${wwn}")
-done
 
-#Ideally, this would have a variable in place of v0,
+# Ideally, this would have a variable in place of v0,
 #  so that it could be used as part of a larger
 #  automation.
-get_hwraid_wwn() {
+hwraid_get_wwn() {
 p /c0/v0 show all |
 sed -n 's/SCSI NAA Id = \(.*\)/\1/p'
 }
 
-raid_clear_arrays() {
+hwraid_clear_virtual_disks() {
 p /c0/vall delete
-}
-
-raid_make_array() {
-	p /c0 add vd \
-	r"${raid_number}" \
-	name=8diskRAID0 \
-	drives=32:"${hwraid_disk_slots}" \
-	pdcache="${pdcache}" \
-	"${writeback}"	\
-	"${readahead}" \
-	"${cachedirect}"
 }
