bd000f397791e4a358b61b8534f2d1740ada632f
Bruxism <Bruxiandee@gmail.com>
2026-06-04 16:01:15 -0500

    temp: this commit shows info intended to be deleted from history at some point

--- lib/lib.fio.benchmark.zfs_functions.sh
+++ lib/lib.fio.benchmark.zfs_functions.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #######################################
-########	ZFS	Functions	###########
+########	 ZFS Config 	###########
 #######################################
 
 zpool_destroy() {
@@ -13,6 +13,42 @@ zpool import -a
 zpool destroy "${zpool_name}"
 wipefs -af /dev/"${drive}"
 blkdiscard -f /dev/"${drive}"
+	zfs_config_declare() {
+	local drive
+	local sda="/dev/disk/by-id/wwn-0x5000cca04daec0b8"
+	local sdb="/dev/disk/by-id/wwn-0x5000cca04db0abb8"
+	local sdc="/dev/disk/by-id/wwn-0x5000cca04db11a94"
+	local sdd="/dev/disk/by-id/wwn-0x5000cca04dac6d70"
+	local sde="/dev/disk/by-id/wwn-0x5000cca072832eb8"
+	local sdf="/dev/disk/by-id/wwn-0x5000cca0729a8b00"
+	local sdg="/dev/disk/by-id/wwn-0x5000cca072837b80"
+	local sdh="/dev/disk/by-id/wwn-0x5000cca07281c3c8"
+	local sdi="/dev/disk/by-id/wwn-0x5000cca072844934"
+	local sdj="/dev/disk/by-id/wwn-0x5000cca0728495a8"
+	local sdk="/dev/disk/by-id/wwn-0x5000cca07283e2bc"
+	local sdl="/dev/disk/by-id/wwn-0x5000cca072836aa8"
+	local sdm="/dev/disk/by-id/wwn-0x5000c5006259e7db"
+
+	declare -g zfs_all_drives_used="$(
+	for drive in $(compgen -A variable sd); do
+		printf '%s ' ${!drive}
+	done
+	)"
+
+	read -ra zfs_all_drives_used <<< "${zfs_all_drives_used}"
+
+	declare -Ag ZPOOL_LAYOUTS=(
+			[SSD_4STRIPE]="12 $sda $sdb $sdc $sdd"
+			[SSD_2X2RAID10]="12 mirror $sda $sdb mirror $sdc $sdd"
+			[SSD_4RAIDZ1]="12 raidz1 $sda $sdb $sdc $sdd"
+			[HDD_8STRIPE]="9 $sde $sdf $sdg $sdh $sdi $sdj $sdk $sdl"
+			[HDD_2X4RAID10]="9 mirror $sde $sdf mirror $sdg $sdh mirror $sdi $sdj mirror $sdk $sdl"
+			[HDD_2MIRROR]="9 $sde $sdf"
+			[HDD_8RAIDZ2]="9 raidz2 $sde $sdf $sdg $sdh $sdi $sdj $sdk $sdl"
+	)
+	}
+	zfs_config_declare
+	declare -p zfs_all_drives_used
 }
 
 zpool_create() {
