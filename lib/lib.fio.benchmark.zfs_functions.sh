c5670a26a11233ae0985094fa2e246bde76012de
Bruxism <Bruxiandee@gmail.com>
2026-06-04 15:43:52 -0500

    fix(test_matrix_zfs): optimize logic of dataset creation by iterating over blocksize and direct first

--- lib/lib.fio.benchmark.zfs_functions.sh
+++ lib/lib.fio.benchmark.zfs_functions.sh
@@ -75,34 +75,57 @@ esac
 }
 
 test_matrix_zfs() {
+# blocksize and direct will determine what options datasets will be created
+#	because those are the primary parameters that chokes or slows down
+#	testfile creation/iteration, and the testfile has to be recreated
+#	each time any time either are set or changed because it changes how
+#	datasets are created--blocksize becomes the recordsize and the value of
+#	direct determines the values of checksum/primarycache
+# Since it can take a long time to recreate the testfile, minimizing the
+#	frequency of that process is accomplished by doing the following:
+# First, all blocksizes are collected from all profiles and combined into
+#	one variable
+# Then, the matrix starts iterating though a for-loop of these combined
+#	list of blocksize, and then and immediately iterates through a for-loop of
+#	direct as either 1 or 0
+# Then, it compares the existence of those iterating sets of blocksize and direct
+#	to see if those exist in the profile being iterated, so that if those don't,
+#	that part of the iteration/loop is skipped/continued
+# This way, all blocksizes are iterated and tested for in their respective,
+#	profiles, and datasets are only recreated when all tests for the values of
+#	blocksize and direct shared in all profiles are completed
 for blocksize in "${!combined_blocksizes[@]}"; do
+for direct in 1 0; do
 for profile in "${fio_profiles[@]}"; do
 	"${profile}"
+	# Skip profiles that don't have matching blocksize
 	[[ " ${blocksizes[@]} " =~ " ${blocksize} " ]] || continue
-	for direct in "${directs[@]}"; do
-		zfs_resolve_direct
-		zfs_create_dataset
-			for ((iodepth_i=0; iodepth_i<"${#iodepths[@]}"; iodepth_i++)); do
-				iodepth="${iodepths[iodepth_i]}"
-				numjobs="${numjobss[-1-iodepth_i]}"
-				for ioengine in "${ioengines[@]}"; do
-					if [[ "${ioengine}" == psync ]]; then
-						gtod_reduce=0
-					else
-						gtod_reduce=1
-					fi
-					for test_type in "${test_types[@]}"; do
-						for size in "${SIZES[@]}"; do
-						for runtime in "${RUNTIMES[@]}"; do
-						for use_pareto in "${use_paretos[@]}"; do
-							fio_function
-						done
-						done
-						done
-					done
-				done
+	# Skip profiles that don't have matching direct
+	[[ " ${directs[@]} " =~ " ${direct} " ]] || continue
+	zfs_clear_testpool_datasets
+	zfs_resolve_direct
+	zfs_create_dataset
+	for ((iodepth_i=0; iodepth_i<"${#iodepths[@]}"; iodepth_i++)); do
+		iodepth="${iodepths[iodepth_i]}"
+		numjobs="${numjobss[-1-iodepth_i]}"
+		for ioengine in "${ioengines[@]}"; do
+			if [[ "${ioengine}" == psync ]]; then
+				gtod_reduce=0
+			else
+				gtod_reduce=1
+			fi
+			for test_type in "${test_types[@]}"; do
+			for size in "${SIZES[@]}"; do
+			for runtime in "${RUNTIMES[@]}"; do
+			for use_pareto in "${use_paretos[@]}"; do
+				fio_function
+			done
+			done
+			done
 			done
 		done
 	done
+done
+done
 done
 }
