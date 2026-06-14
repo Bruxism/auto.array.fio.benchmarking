abcaf4994e1e7a43267c2b0a5b02a8c0db690878
Bruxism <Bruxiandee@gmail.com>
2026-06-14 16:15:46 -0500

    feat!: redo numjob/iodepths iterating drawn as a pair from profile

--- profiles/lib.fio.benchmark.profiles.sh
+++ profiles/lib.fio.benchmark.profiles.sh
@@ -7,8 +7,7 @@
 
 fio_profile_bandwidth() {
 blocksizes=(1MB)
-numjobss=(1 256)
-iodepths=(1 256)
+iodepths_numjobs=(1,256)
 ioengines=(libaio io_uring)
 test_types=(read write)
 directs=(0 1)
@@ -17,8 +16,7 @@ use_paretos=(0 1)
 
 fio_profile_iops() {
 blocksizes=(4kb)
-numjobss=(1 256)
-iodepths=(1 256)
+iodepths_numjobs=(1,256)
 ioengines=(libaio io_uring)
 test_types=(read write randread randrw)
 directs=(0 1)
@@ -27,8 +25,7 @@ use_paretos=(0 1)
 
 fio_profile_latency() {
 blocksizes=(4kb)
-numjobss=(1)
-iodepths=(1)
+iodepths_numjobs=(1,1)
 ioengines=(psync)
 test_types=(randread randrw)
 directs=(1)
