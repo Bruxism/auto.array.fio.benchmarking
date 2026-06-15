5dcb15ac515b68b6eabc8019d1643f6a372c3c7b
Bruxism <Bruxiandee@gmail.com>
2026-06-14 20:38:57 -0500

    wip: add plenty of test items to iterate over

--- profiles/fio.benchmark.profile.exhaustive.sh
+++ profiles/fio.benchmark.profile.exhaustive.sh
@@ -2,12 +2,12 @@
 
 
 #######################################
-########	Profiles	###############
+##########     Profiles      ##########
 #######################################
 
 fio_profile_bandwidth() {
-blocksizes=(1MB)
-iodepths_numjobs=(1,256)
+blocksizes=(64k 128k 1MB)
+iodepths_numjobs=(1,256 256,1 4,64 64,4)
 ioengines=(libaio io_uring)
 test_types=(read write)
 directs=(0 1)
@@ -15,8 +15,8 @@ use_paretos=(0 1)
 }
 
 fio_profile_iops() {
-blocksizes=(4kb)
-iodepths_numjobs=(1,256)
+blocksizes=(4kb 8k 16k)
+iodepths_numjobs=(1,256 256,1 4,64 64,4)
 ioengines=(libaio io_uring)
 test_types=(read write randread randrw)
 directs=(0 1)
