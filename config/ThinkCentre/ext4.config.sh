bd8874f4c05ca37bc68d0ab3ce09bb72d185c5e6
Bruxism <Bruxiandee@gmail.com>
2026-06-16 23:16:28 -0500

    temp: ThinkCentre ext4.config

---
+++ config/ThinkCentre/ext4.config.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+declare -A EXT4_LAYOUTS
+#######################################
+############  Ext4 Config  ############
+#######################################
+
+EXT4_LAYOUTS=('/dev/disk/by-id/wwn-0x33333330000007d0')
+readonly EXT4_LAYOUTS
