37a87f3922a3cf634923f64905f1755e90af029f
Bruxism <Bruxiandee@gmail.com>
2026-06-16 23:15:50 -0500

    feature: add test matrices for ext4 by itself

--- lib/lib.fio.benchmark.ext4_functions.sh
+++ lib/lib.fio.benchmark.ext4_functions.sh
@@ -31,3 +31,54 @@ sleep 1
 blkdiscard -f "${testdisk_by_id}" 2> /dev/null
 sleep 1
 }
+
+ext4_disk_matrix() {
+local testdisk_by_id
+local test_partition
+local test_mountdir
+local testfile
+local disk_config
+local results_dir="${RESULTS_DIR}/ext4/"
+
+mkdir -p "${results_dir}"
+
+sleep 1
+for testdisk_by_id in "${EXT4_LAYOUTS[@]}"; do
+	disk_config="${hwraid_array}"
+	wipefs -af "${testdisk_by_id}"
+	ext4_make
+	ext4_test_matrix
+	ext4_delete
+done
+}
+
+ext4_test_matrix() {
+local iodepth_numjob
+
+for profile in "${fio_profiles[@]}"; do
+	"${profile}"
+	for direct in "${directs[@]}"; do
+	for blocksize in "${blocksizes[@]}"; do
+		for iodepth_numjob in "${iodepths_numjobs[@]}"; do
+			IFS="," read -r iodepth numjobs <<< "$(echo "${iodepth_numjob}")"
+			for ioengine in "${ioengines[@]}"; do
+				if [[ "${ioengine}" == psync ]]; then
+					gtod_reduce=0
+				else
+					gtod_reduce=1
+				fi
+				for test_type in "${test_types[@]}"; do
+					for size in "${SIZES[@]}"; do
+					for runtime in "${RUNTIMES[@]}"; do
+					for use_pareto in "${use_paretos[@]}"; do
+						fio_function
+					done
+					done
+					done
+				done
+			done
+		done
+	done
+	done
+done
+}
