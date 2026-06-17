81dad8c70d896bc9a7bd63e329c38f86def73b41
Bruxism <Bruxiandee@gmail.com>
2026-06-16 20:38:58 -0500

    temp: add personal ThinkCentre zfs config

---
+++ config/ThinkCentre/zfs.config.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+declare -A ZFS_ZPOOL_LAYOUTS
+declare -A ZFS_DEVICES
+#######################################
+############# ZFS Config ##############
+#######################################
+
+# Device name to absolute WWN path
+#	Device name doesn't have to match actual path
+#	This is mostly for convenience
+# This is used to convert names in ZFS_ZPOOL_LAYOUTS and
+#	for wiping drives
+ZFS_DEVICES=(
+	[sda]="/dev/disk/by-id/wwn-0x33333330000007d0"
+)
+readonly ZFS_DEVICES
+
+ZFS_ZPOOL_LAYOUTS=(
+	[SSD_1STRIPE]="9 sda"
+)
+# Change ZFS_ZPOOL_LAYOUTS device names to WWN paths
+for pool in "${!ZFS_ZPOOL_LAYOUTS[@]}"; do
+    new_layout=()
+    for token in ${ZFS_ZPOOL_LAYOUTS["${pool}"]}; do
+        new_layout+=("${ZFS_DEVICES[$token]:-$token}")
+    done
+    ZFS_ZPOOL_LAYOUTS["${pool}"]="${new_layout[*]}"
+	unset pool new_layout token
+done
+readonly ZFS_ZPOOL_LAYOUTS
+declare -p ZFS_ZPOOL_LAYOUTS
