7a4bb5b1c9723737918012d11ca18c8b246805fb
Bruxism <Bruxiandee@gmail.com>
2026-07-07 17:21:36 -0500

    feat: streamline testing such that the whole script can be called and then each function for each kind of test

--- scratchpad/Manual.Switching.Tests.sh
+++ scratchpad/Manual.Switching.Tests.sh
@@ -3,15 +3,11 @@
 chmod +x fio.ZFS.Ext4.hwRAID.TestSuite.sh
 source fio.ZFS.Ext4.hwRAID.TestSuite.sh PE.2StripeSplit pareto
 alias f=fio_function
-declare -n output=output_name
 alias r='less ${output}.log'
+declare -n output=output_name
 
-
-
-disk_config=HW_SSD_2STRIPE				# Needed
-
-size=50GB
-runtime=300
+size=30GB
+runtime=90
 test_type=randread
 direct=1								# Needed
 blocksize=1MB
@@ -21,6 +17,9 @@ ioengine=io_uring
 gtod_reduce=1
 # mem_align=512b
 use_pareto=1
+
+prep_hwraid () {
+disk_config=HW_SSD_2STRIPE				# Needed
 
 results_dir="${RESULTS_DIR}/HWRAID"
 mkdir -p "${results_dir}"
@@ -39,37 +38,16 @@ p /c0 show all > "${results_dir}"/controller_info.txt
 p /c0/e32/sall show all > "${results_dir}"/drives_all_info.txt
 
 unset mem_align
-
-alias f=fio_function
-
 #testfile="${test_mountdir}/"	# for use with --directory
+}
 
-
-
-
-
-
+watch_color () {
 export S_COLORS=always
 watch  -n1 iostat -y --human 1 1
+}
 
-
-
-wipefs -af
-
+prep_zfs () {
 disk_config=ZFS_SSD_2STRIPE				# Needed
-
-size=50GB
-runtime=300
-test_type=randread
-direct=0								# Needed
-blocksize=1MB
-iodepth=1
-numjobs=20
-ioengine=io_uring
-gtod_reduce=1
-# mem_align=512b
-use_pareto=1
-
 
 declare -n zpool_name=disk_config
 results_dir="${RESULTS_DIR}/zfs"
@@ -101,8 +79,10 @@ zfs_clear_testpool_datasets
 zfs_create_dataset
 testfile="/${zpool_name}/${blocksize}/testfile"
 #testfile="/${zpool_name}/${blocksize}/"
+}
 
 
+prep_unused () {
 zfs_clear_test_drives
 hwraid_activate_disks
 hwraid_add_virtual_disk
@@ -119,41 +99,19 @@ mkdir -p "${results_dir}"
 
 
 
-
-
-
-
-
-
-
 disk_config=ZFS_SSD_2STRIPE_ZVOL"${blocksize}"
-
-
-size=50GB
-runtime=120
-test_type=randwrite
-direct=0
-blocksize=8MB
-iodepth=8
-numjobs=4
-ioengine=io_uring
-gtod_reduce=0
-# mem_align=512b
-use_pareto=1
-
-
-
+}
 
 
 # For fio_function_distributed
-size=4k-32MB
-runtime=180
-test_type=readwrite
-direct=0
-blocksize=128k
-iodepth=64
-numjobs=1
-ioengine=io_uring
-gtod_reduce=0
-# mem_align=512b
-use_pareto=0
+# size=4k-32MB
+# runtime=180
+# test_type=readwrite
+# direct=0
+# blocksize=128k
+# iodepth=64
+# numjobs=1
+# ioengine=io_uring
+# gtod_reduce=0
+# # mem_align=512b
+# use_pareto=0
