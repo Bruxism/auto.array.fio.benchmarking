cbd576bbf3d38f38fc51872a93a8ecb2b6a02bc3
Bruxism <Bruxiandee@gmail.com>
2026-07-13 14:39:05 -0500

    temp: ext4 config for personal Dell PowerEdge

---
+++ config/PowerEdge730xd/ext4.config.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+declare -A EXT4_LAYOUTS
+#######################################
+############  Ext4 Config  ############
+#######################################
+
+EXT4_LAYOUTS=(
+	[sda]="/dev/disk/by-id/wwn-0x5000cca04daec0b8"
+	[sdb]="/dev/disk/by-id/wwn-0x5000cca04db0abb8"
+	[sdc]="/dev/disk/by-id/wwn-0x5000cca04db11a94"
+	[sdd]="/dev/disk/by-id/wwn-0x5000cca04dac6d70"
+	[sde]="/dev/disk/by-id/wwn-0x5000cca072832eb8"
+	[sdf]="/dev/disk/by-id/wwn-0x5000cca0729a8b00"
+	[sdg]="/dev/disk/by-id/wwn-0x5000cca072837b80"
+	[sdh]="/dev/disk/by-id/wwn-0x5000cca07281c3c8"
+	[sdi]="/dev/disk/by-id/wwn-0x5000cca072844934"
+	[sdj]="/dev/disk/by-id/wwn-0x5000cca0728495a8"
+	[sdk]="/dev/disk/by-id/wwn-0x5000cca07283e2bc"
+	[sdl]="/dev/disk/by-id/wwn-0x5000cca072836aa8"
+	[sdm]="/dev/disk/by-id/wwn-0x5000c5006259e7db"
+)
