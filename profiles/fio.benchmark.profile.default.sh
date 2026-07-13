5a379ab71e42b367e6e9f19fc3df15756dee2ddb
Bruxism <Bruxiandee@gmail.com>
2026-07-13 14:57:51 -0500

    fix(profile): adjusted 'default' profile and the note about it

--- profiles/fio.benchmark.profile.default.sh
+++ profiles/fio.benchmark.profile.default.sh
@@ -11,12 +11,12 @@ iodepths_numjobs=(20,1)
 ioengines=(io_uring)
 test_types=(read write)
 directs=(0 1)
-use_paretos=(0 1)
+use_paretos=(0)
 }
 
 fio_profile_bandwidth_random () {
 blocksizes=(64k 128k 1MB)
-iodepths_numjobs=(1,20)
+iodepths_numjobs=(20,1)
 ioengines=(io_uring)
 test_types=(randread randwrite randrw)
 directs=(0 1)
@@ -29,14 +29,14 @@ iodepths_numjobs=(20,1)
 ioengines=(io_uring)
 test_types=(read write)
 directs=(0 1)
-use_paretos=(0 1)
+use_paretos=(0)
 }
 
 fio_profile_iops_random () {
 blocksizes=(4kb 8k 16k)
-iodepths_numjobs=(1,20)
+iodepths_numjobs=(20,1)
 ioengines=(io_uring)
-test_types=(randread randrw)
+test_types=(randread randwrite randrw)
 directs=(0 1)
 use_paretos=(0 1)
 }
