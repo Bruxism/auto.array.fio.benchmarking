a101c9aae394615805ad5e1122efe703361b598e
Bruxism <Bruxiandee@gmail.com>
2026-08-31 17:59:51 -0500

    chore: untrack my own personal configs for my system from the repo
    I've decided that the content isn't secret, so I don't mind
    that the content is in the history; but for the sake of
    presentation, I'm going to remove them from appearing in HEAD
    from here on.

--- config/ThinkCentre/ext4.config.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-declare -A EXT4_LAYOUTS
-#######################################
-############  Ext4 Config  ############
-#######################################
-
-EXT4_LAYOUTS=(
-	[NVMe]='/dev/disk/by-id/nvme-eui.e8238fa6bf530001001b448b4ebdf8cc'
-)
--- config/ThinkCentre/raw.config.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-declare -A RAW_LAYOUTS
-#######################################
-############  RAW Config  #############
-#######################################
-
-RAW_LAYOUTS=(
-	[NVMe]="/dev/disk/by-id/nvme-eui.e8238fa6bf530001001b448b4ebdf8cc"
-)
